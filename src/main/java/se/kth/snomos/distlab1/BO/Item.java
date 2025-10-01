e735e87eff3952ebb78cd2699ea56bdfbbd54295
Johan Karlsson <johankarlsson3125@gmail.com>
2025-10-01 18:30:16 +0200

    Item and ItemDB added

---
+++ src/main/java/se/kth/snomos/distlab1/BO/Item.java
@@ -0,0 +1,19 @@
+package se.kth.snomos.distlab1.BO;
+
+public class Item {
+    private int id;
+    private String name;
+    private double price;
+    private int stock;
+
+    public Item(int id, String name, double price, int stock) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public Item() {}
+
+
+}
---
+++ src/main/java/se/kth/snomos/distlab1/DB/ItemDB.java
@@ -0,0 +1,10 @@
+package se.kth.snomos.distlab1.DB;
+
+import se.kth.snomos.distlab1.BO.Item;
+
+public class ItemDB extends Item {
+
+    protected ItemDB(int id, String name, double price, int stock){
+        super(id, name, price, stock);
+    }
+}
