9963af57829afae876503adff26f10d7d7136564
eliasBossen <eliapa@kth.se>
2025-10-01 13:17:47 +0200

    Connection to database

--- src/main/java/se/kth/snomos/distlab1/DB/DBManager.java
+++ src/main/java/se/kth/snomos/distlab1/DB/DBManager.java
@@ -1,4 +1,29 @@
 package se.kth.snomos.distlab1.DB;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBManager {
+    private static DBManager instance = null;
+    private Connection connection = null;
+
+    private static DBManager getInstance(){
+        if(instance == null){
+            instance = new DBManager();
+        }
+        return instance;
+    }
+
+    private DBManager(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/?user=root");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection(){
+        return getInstance().connection;
+    }
 }
